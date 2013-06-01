892953c43694814dcf2295d0422f7df296065306
Jose Ruzafa <jose.ruzafa@gmail.com>
2013-06-01 20:15:45 +0200

    added database name

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
+			'database'  => 'time-tracker',
+			'username'  => 'zend',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
