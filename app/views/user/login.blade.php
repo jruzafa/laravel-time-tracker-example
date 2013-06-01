e28af159291eef30f8dd3775d5af11b1a4b936cd
Jose Ruzafa <jose.ruzafa@gmail.com>
2013-06-01 23:16:32 +0200

    Template de login

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.template')
 
 @section('content')
-<form class="form-signin">
-	<h2 class="form-signin-heading">Please sign in</h2>
-	<input type="text" class="input-block-level" placeholder="Email address">
-	<input type="password" class="input-block-level" placeholder="Password">
-	<label class="checkbox">
-		<input type="checkbox" value="remember-me"> Remember me
-	</label>
-	<button class="btn btn-large btn-primary" type="submit">Sign in</button>
-</form>
+	@if (Session::has('login_errors'))
+		<span class="error">Username or password incorrect.</span>
+	@endif
+
+	{{ Form::open(array('class' => 'form-signin')) }}
+
+		<h2 class="form-signin-heading">Please sign in</h2>
+
+		{{ Form::text('email','', array('class' => 'input-block-level', 'placeholder' => 'Email address')) }}
+		{{ Form::password('password',array('class' => 'input-block-level', 'placeholder' => 'Password')) }}
+		<label class="checkbox">
+			<input type="checkbox" value="remember-me"> Remember me
+		</label>
+		{{ Form::button('Login', array('class' => 'btn btn-large btn-primary', 'type' => 'submit')) }}
+	{{ Form::close() }}
 @stop
