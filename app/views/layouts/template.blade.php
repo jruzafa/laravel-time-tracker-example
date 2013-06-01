6962e69a1c0765f8b75f288bf86119cbb150f021
Jose Ruzafa <jose.ruzafa@gmail.com>
2013-06-01 21:27:31 +0200

    Template app

---
+++ app/views/layouts/template.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        {{ HTML::style('css/bootstrap.min.css') }}
+        <style>
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+
+        {{ HTML::style('css/bootstrap-responsive.min.css') }}
+        {{ HTML::style('css/main.css') }}
+
+        <?php echo HTML::script('js/vendor/modernizr-2.6.2-respond-1.1.0.min.js'); ?>
+    </head>
+    <body>
+
+        <div class="container-fluid">
+
+            <div class="row-fluid">
+                @yield('content')
+            </div>
+
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <!-- <script>window.jQuery || document.write(<?php //echo HTML::script('js/vendor/jquery-1.9.1.min.no-js'); ?>)</script> -->
+
+        <?php echo HTML::script('js/vendor/bootstrap.min.js'); ?>
+
+        <?php echo HTML::script('js/main.js'); ?>
+
+    </body>
+</html>
