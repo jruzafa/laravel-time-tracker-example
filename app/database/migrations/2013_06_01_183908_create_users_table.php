6e34cfb90f4a9fd0b7be817b44e2befee338b8cf
Jose Ruzafa <jose.ruzafa@gmail.com>
2013-06-01 21:26:53 +0200

    Migration user table

---
+++ app/database/migrations/2013_06_01_183908_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('users', function($table){
+
+			$table->increments('id');
+			$table->string('email');
+			$table->string('password');
+			$table->boolean('active');
+			$table->timestamps();
+
+			$table->unique('email');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::dropIfExists('users');
+	}
+
+}
