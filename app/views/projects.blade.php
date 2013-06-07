060229fc6a3dbca05de6ef2bf672fb0d64e11315
Jose Ruzafa <jose.ruzafa@gmail.com>
2013-06-07 07:49:25 +0200

    Refactor the structure layout of projects.

--- app/views/projects.blade.php
+++ app/views/projects.blade.php
@@ -4,46 +4,51 @@
 
 	@if (count($errors) > 0)
 
-		@foreach ($errors->all() as $message)
-
-		<div class="alert alert-error">
-              <button data-dismiss="alert" class="close" type="button">×</button>
-              <strong>{{ $message }}</strong>
-            </div>
-		@endforeach
-
+		<div class="row-fluid">
+			<div class="span12">
+					@foreach ($errors->all() as $message)
+					<div class="alert alert-error">
+					<button data-dismiss="alert" class="close" type="button">×</button>
+					<strong>{{ $message }}</strong>
+					</div>
+					@endforeach
+			</div>
+		</div>
 	@endif
 
-	{{ Form::open(array('class' => 'form-signin')) }}
+	<div class="row-fluid">
+		<div class="span12">
 
-		<h2>Create project</h2>
+			{{ Form::open(array('class' => 'form-signin')) }}
 
-		{{ Form::text('name','', array('class' => 'input-block-level', 'placeholder' => 'Name of the project')) }}
-
-		{{ Form::button('Create', array('class' => 'btn btn-large btn-primary', 'type' => 'submit')) }}
-
-	{{ Form::close() }}
-
+				<h2>Create project</h2>
+				{{ Form::text('name','', array('class' => 'input-block-level', 'placeholder' => 'Name of the project')) }}
+				{{ Form::button('Create', array('class' => 'btn btn-large btn-primary', 'type' => 'submit')) }}
+			{{ Form::close() }}
+		</div>
+	</div>
 
 	@if (count($projects) > 0)
-		<table class="table table-hover span5">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Hours</th>
-                </tr>
-              </thead>
-              <tbody>
-				@foreach ($projects as $project)
-					<tr>
-						<td>{{ $project->name }}</td>
-						<td>0</td>
-					</tr>
 
-				@endforeach
-              </tbody>
-            </table>
+		<div class="row-fluid">
+			<div class="span12">
+				<table class="table table-hover span5">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Hours</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($projects as $project)
+							<tr>
+								<td>{{ $project->name }}</td>
+								<td>0</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
 	@endif
-
-
 @stop
