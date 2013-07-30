ffee2c9ac677be34f5918ecf3f7616b0ce1e48f2
Jose Ruzafa <jose.ruzafa@gmail.com>
2013-07-30 14:16:56 +0200

    Relations Time

--- app/models/Time.php
+++ app/models/Time.php
@@ -2,4 +2,14 @@
 
 class Time extends Eloquent{
 
+    // Except id for Project::create
+    protected $guarded = array('id');
+
+    public function user(){
+        return $this->belongsTo('User');
+    }
+
+    public function project(){
+        return $this->belongsTo('Project');
+    }
 }
